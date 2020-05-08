8cfd69821dedbfba77915ceda2f7b8eb314d497c
LambdaXymox <lambda.xymox@gmail.com>
2020-05-08 01:48:18 -0500

    Add benchmark case.

---
+++ benches/benchmark_teapot.rs
@@ -0,0 +1,20 @@
+use criterion::{
+    black_box, 
+    criterion_group, 
+    criterion_main, 
+    Criterion
+};
+use wavefront_exp as obj;
+
+const SAMPLE_DATA: &str = "assets/teapot.obj";
+
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("parse teapot.obj", |b| b.iter(|| {
+        let result = obj::parser1::parse_file(black_box(SAMPLE_DATA));
+        result.unwrap()
+    }));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
