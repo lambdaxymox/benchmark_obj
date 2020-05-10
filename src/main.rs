6da792817b095e5206d13ae926ef2e1338d9c2f5
LambdaXymox <lambda.xymox@gmail.com>
2020-05-09 19:10:15 -0500

    Update main.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,12 @@ pub mod lexer2;
 pub mod parser1;
 pub mod parser2;
 
-use crate::lexer::Lexer;
-use std::iter;
-use std::error;
-use std::fmt;
-use std::io::{BufReader, Read};
-use std::fs::File;
-use std::path::Path;
-
 
 const SAMPLE_DATA: &str = "assets/teapot.obj";
 
 
 fn main() {
     println!("BEGING PARSING.");
-    let result: parser2::Object = parser2::parse_file(SAMPLE_DATA).unwrap();
+    parser2::parse_file(SAMPLE_DATA).unwrap();
     println!("END PARSING.");
 }
